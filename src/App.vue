9da5c59c178214f58787d3432793f33a1e5d3ce3
aelmore49 <alexelmore49@gmail.com>
2023-02-20 09:52:42 -0600

    first Vue 3 setup() assignment with my answers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
+
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <div v-if="showGoals">{{ myGoals.goal }}</div>
+  <h3>OUTPUT COURSE GOAL</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoals">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref, reactive } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const myGoals = reactive({
+      goal: "Become a better front end developer",
+    });
+
+    const showGoals = ref(false);
+
+    function toggleGoals() {
+      showGoals.value = !showGoals.value;
+    }
+
+    return {
+      myGoals,
+      showGoals,
+      toggleGoals,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
